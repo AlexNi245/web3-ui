9275eec18344759fefd644f4a34012c97c442068
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-12-03 13:52:19 +0530

    refactor(NFT): accept contractAddress and tokenId to <NFT />

--- packages/components/src/components/NFT/NFT.tsx
+++ packages/components/src/components/NFT/NFT.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Box, Heading, Image, Flex, Tag, Text } from '@chakra-ui/react';
+import fetch from 'cross-fetch';
 
 export interface NFTProps {
+  contractAddress: string;
   tokenId: string;
-  name: string | null;
+}
+
+interface NFTData {
+  tokenId: string;
   imageUrl: string;
-  assetContractName: string;
+  name: string;
   assetContractSymbol: string;
+  assetContractName: string;
 }
 
 /**
@@ -15,14 +21,31 @@ export interface NFTProps {
  * TODO: Add a component that fetchs the NFT data from the blockchain and uses this component to
  * display it
  */
-export const NFT = ({
-  tokenId,
-  name,
-  imageUrl,
-  assetContractName,
-  assetContractSymbol,
-}: NFTProps) => {
-  const displayName = name || tokenId;
+export const NFT = ({ contractAddress, tokenId }: NFTProps) => {
+  const [nftData, setNftData] = React.useState<NFTData>();
+
+  const fetchNFTData = useCallback(async () => {
+    const response = await (
+      await fetch(`https://api.opensea.io/api/v1/asset/${contractAddress}/${tokenId}/`)
+    ).json();
+    setNftData({
+      tokenId: response.tokenId,
+      imageUrl: response.image_url,
+      name: response.name,
+      assetContractName: response.asset_contract.name,
+      assetContractSymbol: response.asset_contract.symbol,
+    });
+  }, [contractAddress, tokenId]);
+
+  useEffect(() => {
+    fetchNFTData();
+  }, [contractAddress, tokenId]);
+
+  if (!nftData) {
+    return <div>Loading...</div>;
+  }
+
+  const { name: displayName, imageUrl, assetContractName, assetContractSymbol } = nftData;
 
   return (
     <Box maxW='xs' borderRadius='lg' borderWidth='1px' overflow='hidden'>
